2b58fb76d547cb388c6d98377342047bf20af3d3
TanishqSingla <tanishqsingla00@gmail.com>
2023-08-30 02:47:58 +0530

    feat: Add sqlite3 setup
    Add put api for lists

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/TanishqSingla/go-native-todo-server
+
+go 1.21
+
+require (
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+)
--- main.go
+++ main.go
@@ -1,45 +1,143 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // For handling dynamic routing
-type ListHandler struct {}
+type ListHandler struct{}
+
 func (p *ListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  id := r.URL.Path[6:]
+	id := r.URL.Path[6:]
 
-  if r.Method == "GET" {
-    fmt.Fprintf(w, "opening %s...", id)
-    return
-  }
+	if r.Method == "GET" {
+		fmt.Fprintf(w, "opening %s...", id)
+		return
+	}
 
-  http.NotFound(w, r)
+	http.NotFound(w, r)
 	return
 }
 
-type TodoHandler struct {}
+type TodoHandler struct{}
+
+type List struct {
+	id          string
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type Todo struct {
+	id          string
+	Description string `json:"description"`
+	ListId      string `json:"listId"`
+}
+
 func (p *TodoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  id := r.URL.Path[6:]
+	id := r.URL.Path[6:]
 
-  if r.Method == "GET" {
-    fmt.Fprintf(w, "opening todo %s...", id)
-    return
-  }
+	if r.Method == "GET" {
+		fmt.Fprintf(w, "opening todo %s...", id)
+		return
+	}
 
-  http.NotFound(w, r)
-  return
+	http.NotFound(w, r)
+	return
+}
+
+func errorResponse(w http.ResponseWriter, message string, httpStatusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatusCode)
+	response := map[string]interface{}{
+		"message": message,
+		"status":  httpStatusCode,
+		"error":   http.StatusText(httpStatusCode),
+	}
+
+	jsonResponse, err := json.Marshal(response)
+
+	if err != nil {
+		log.Fatal("Invalid json")
+	}
+
+	w.Write(jsonResponse)
 }
 
 func main() {
 	newMux := http.NewServeMux()
 
-	newMux.HandleFunc("/lists", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			return
+	db, err := sql.Open("sqlite3", "todo.db")
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if db != nil {
+		log.Println("connected to database")
+	}
+
+	initTableQuery := `
+CREATE TABLE IF NOT EXISTS lists (name TEXT, description TEXT, id TEXT PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS todos (id TEXT PRIMARY KEY, description TEXT, listId TEXT, FOREIGN KEY(listId) REFERENCES lists(id));
+`
+
+	_, initDbError := db.Exec(initTableQuery)
+
+	if initDbError != nil {
+		log.Fatal(initDbError.Error())
+	}
+
+	newMux.HandleFunc("/lists/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path[6:]
+
+		if path == "/" {
+			if r.Method == "GET" {
+				return
+			}
 		}
+
+		if path == "/add" {
+			if r.Method == http.MethodPut {
+				contentType := r.Header.Get("Content-Type")
+
+				if contentType != "application/json" {
+					errorResponse(w, "Content Type is not JSON", http.StatusUnprocessableEntity)
+					return
+				}
+
+				var newList List
+
+				decoder := json.NewDecoder(r.Body)
+				decoder.DisallowUnknownFields()
+				err := decoder.Decode(&newList)
+
+				if err != nil {
+					errorResponse(w, "Bad Request", http.StatusBadRequest)
+					return
+				}
+
+				createListQuery := fmt.Sprintf(`INSERT INTO lists (name, description, id) VALUES('%s', '%s', '%s')`, newList.Name, newList.Description, uuid.NewString())
+
+				_, dbErr := db.Exec(createListQuery)
+
+				if dbErr != nil {
+					errorResponse(w, "Internal Server Error", http.StatusInternalServerError)
+					log.Fatal(err.Error())
+					return
+				}
+
+				return
+			}
+		}
+
 		http.NotFound(w, r)
 		return
 	})
@@ -63,7 +161,7 @@ func main() {
 		return
 	})
 
-  newMux.Handle("/todo/", &TodoHandler{})
+	newMux.Handle("/todo/", &TodoHandler{})
 
 	log.Fatal(http.ListenAndServe(":4000", newMux))
 }
